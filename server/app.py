539bbfbe902692b7a984cfa5c6731bee98a03b50
james <omarejames082@gmail.com>
2024-07-27 02:46:37 -0700

    done

--- server/app.py
+++ server/app.py
@@ -1,33 +1,69 @@
+#!/usr/bin/env python
 #!/usr/bin/env python3
 
-from flask import request, session
-from flask_restful import Resource
-from sqlalchemy.exc import IntegrityError
+from flask import Flask, jsonify, request, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
-from config import app, db, api
-from models import User, Recipe
+from models import db, Plant
 
-class Signup(Resource):
-    pass
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-class CheckSession(Resource):
-    pass
+migrate = Migrate(app, db)
+db.init_app(app)
 
-class Login(Resource):
-    pass
+api = Api(app)
 
-class Logout(Resource):
-    pass
+class Plants(Resource):
 
-class RecipeIndex(Resource):
-    pass
+    def get(self):
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plants), 200)
 
-api.add_resource(Signup, '/signup', endpoint='signup')
-api.add_resource(CheckSession, '/check_session', endpoint='check_session')
-api.add_resource(Login, '/login', endpoint='login')
-api.add_resource(Logout, '/logout', endpoint='logout')
-api.add_resource(RecipeIndex, '/recipes', endpoint='recipes')
+    def post(self):
+        data = request.get_json()
 
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price'],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(new_plant.to_dict(), 201)
+api.add_resource(Plants, '/plants')
+
+class PlantByID(Resource):
+    def get(self, id):
+        plant = Plant.query.filter_by(id=id).first().to_dict()
+        return make_response(jsonify(plant), 200)
+
+    def patch(self, id):
+        data = request.get_json()
+
+        plant = Plant.query.filter_by(id=id).first()
+
+        for attr in data:
+            setattr(plant, attr, data[attr])
+
+        db.session.add(plant)
+        db.session.commit()
+
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response('', 204)
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
